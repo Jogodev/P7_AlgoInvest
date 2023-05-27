e5384fcb31738862524487a97691179c9284e71a
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2023-05-27 21:00:22 +0200

    algo bruteforce version 1

--- bruteforce.py
+++ bruteforce.py
@@ -1,35 +1,37 @@
 from itertools import combinations
+from tqdm import tqdm
 import time
 import csv
 
 start_time = time.time()
 
-file_csv = "csv/20_actions.csv"
+file_csv = "csv/20_shares.csv"
 max_budget = 500
 
 
 # lire mon csv et créer une liste de dict
 def read_file(path):
-    actions = []
+    shares = []
     with open(path, mode='r') as csv_file:
         csv_line = csv.DictReader(csv_file)
-        for action in csv_line:
-            action['price'] = float(action['price'])
-            action['profit'] = (action['price'] * float(action['profit']) / 100)
-            actions.append(action)
-    return actions
+        for share in csv_line:
+            share['price'] = float(share['price'])
+            share['profit'] = (share['price'] * float(share['profit']) / 100)
+            shares.append(share)
+    return shares
 
 
 # Créer la liste de toutes les combinaisons
-def get_best_combinations(actions):
+def get_best_combinations(shares):
     best_profit = 0
     best_combination = []
-    for action in range(len(actions)):
-        for combination in combinations(actions, action):
-            total_price = sum([action['price'] for action in combination])
+    for share in tqdm(range(len(shares))):
+        for combination in combinations(shares, share):
+            total_price = sum([share['price'] for share in combination])
             if total_price <= max_budget:
-                total_profit = sum(action['profit'] for action in combination)
+                total_profit = sum(share['profit'] for share in combination)
                 if total_profit > best_profit:
+                    best_profit = total_profit
                     best_combination = [total_price, total_profit, combination]
     return best_combination
 
@@ -38,18 +40,19 @@ def result_display(best_combinations):
     total_price = best_combinations[0]
     total_profit = best_combinations[1]
     combination = list(best_combinations[2])
-    combination.sort(key=lambda action: action['profit'], reverse=True)
-    for action in combination:
-        print(f"{action['name']} | {action['price']} | {action['profit']} €")
+    combination.sort(key=lambda share: share['profit'], reverse=True)
+    for share in combination:
+        print(f"{share['name']} | {share['price']} | {share['profit']} €")
 
+    print(f"\nNombre d'shares à achetés : {len(share['name'])}")
     print(f"\nPrix total : {total_price}", "€")
     print(f"\nBenefice après 2 ans : {total_profit}", "€")
     print("\nTemps d'éxecution : ", time.time() - start_time, "secondes")
 
 
 def main():
-    actions = read_file(file_csv)
-    best_combination = get_best_combinations(actions)
+    shares = read_file(file_csv)
+    best_combination = get_best_combinations(shares)
     result_display(best_combination)
 
 
