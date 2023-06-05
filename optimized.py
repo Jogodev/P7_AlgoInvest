3df783489f840cc07a13b9a498744dc467e7a5de
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2023-06-05 15:55:41 +0200

    optimized ok

--- optimized.py
+++ optimized.py
@@ -1,10 +1,12 @@
-from tqdm import tqdm
 import time
+
+from tqdm import tqdm
+
 import csv
 
 start_time = time.time()
-file_csv = "csv/dataset1_Python+P7.csv"
-# file_csv = "csv/dataset2_Python+P7.csv"
+# file_csv = "csv/dataset1_Python+P7.csv"
+file_csv = "csv/dataset2_Python+P7.csv"
 # file_csv = "csv/20_shares.csv"
 MAX_BUDGET = 500
 
@@ -15,63 +17,58 @@ def read_file(path):
     with open(path, mode='r') as csv_file:
         csv_line = csv.DictReader(csv_file)
         for share in csv_line:
-            name = share['name']
-            price = int(float(share['price']) * 100)
-            profit = round((float(share['profit']) / 100) * (float(share['price']) * 100), 2)
-            if float(share['price']) > 0:
+            if float(share['price']) > 0 and float(share['profit']) > 0:
+                name = share['name']
+                price = int(float(share['price']) * 100)
+                profit = round((float(share['profit']) / 100) * (float(share['price']) * 100), 2)
                 share = {"name": name, "price": price, "profit": int(profit)}
-            shares.append(share)
+                shares.append(share)
     return shares
 
 
 def knapsack(shares):
     budget = int(MAX_BUDGET * 100)
-    all_shares = len(shares)
-    matrix = [[0 for x in range(budget + 1)] for x in range(all_shares + 1)]
+    matrix = [[0 for x in range(budget + 1)] for x in range(len(shares) + 1)]
 
-    for i in tqdm(range(1, all_shares + 1)):
+    for i in tqdm(range(1, len(shares) + 1)):
         for j in range(1, budget + 1):
             if shares[i - 1]['price'] <= j:
                 matrix[i][j] = max(
                     shares[i - 1]['profit'] + matrix[i - 1][j - shares[i - 1]['price']], matrix[i - 1][j])
             else:
                 matrix[i][j] = matrix[i - 1][j]
-
-
-    n = all_shares
+    n = len(shares)
     best_combination = []
     while budget > 0 and n > 0:
         share = shares[n - 1]
-        if matrix[n][budget] == matrix[n - 1][budget - share['price'] + share['profit']]:
+        if matrix[n][budget] == matrix[n - 1][budget - share['price']] + share['profit']:
             best_combination.append(share)
             budget -= share['price']
         n -= 1
 
-    return (matrix[-1][-1] / 100), best_combination
+    total_price = sum([share['price'] for share in best_combination])
+    return [(matrix[-1][-1] / 100), total_price, best_combination]
 
 
-# def result_display(best_combination):
-#     total_price = []
-#     total_profit = []
-#     combination = list(best_combination)
-#     # combination.sort(key=lambda share: share['profit'], reverse=True)
-#     for share in combination:
-#         print(f"{share['name']} | {share['price']} | {share['profit']} €")
-#         total_price.append(share['price'] / 100)
-#         total_profit.append(share['profit'])
-#
-#     print(f"\nNombre d'actions à achetés : {len(combination)}")
-#     print(f"\nPrix total : ", sum(total_price), "€")
-#     # print(f"\nBenefice après 2 ans en pourcentage: {str(total_profit / total_price * 100)[:5]}", "%")
-#     print(f"\nBenefice après 2 ans : ", sum(total_profit), "€")
-#     print("\nTemps d'éxecution : ", str(time.time() - start_time)[:4], "secondes")
+def result_display(best_combinations):
+    total_profit = best_combinations[0]
+    total_price = best_combinations[1]
+    combination = list(best_combinations[2])
+    combination.sort(key=lambda share: share['profit'], reverse=True)
+    for share in combination:
+        print(f"{share['name']} | {share['price'] / 100} | {share['profit'] / 100} €")
+
+    print(f"\nNombre d'actions à achetés : {len(combination)}")
+    print(f"\nPrix total : {round(total_price / 100, 2)}", "€")
+    print(f"\nBenefice après 2 ans en pourcentage: {str((total_profit / total_price * 100) * 100)[:5]}", "%")
+    print(f"\nBenefice après 2 ans : {round(total_profit, 2)}", "€")
+    print("\nTemps d'éxecution : ", str(time.time() - start_time)[:4], "secondes")
 
 
 def main():
     shares = read_file(file_csv)
-    print(knapsack(shares))
-    # best_combination = knapsack(shares)
-    # result_display(best_combination)
+    best_combination = knapsack(shares)
+    result_display(best_combination)
 
 
 if __name__ == '__main__':
